de7f2df057f058aee485a6fcac15d22ad3f60349
zongxili <zongxiliwork@gmail.com>
2019-10-15 10:27:23 -0700

    task 2 done

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,10 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+@store1 = Store.find_by(id: 1)
+@store2 = Store.find_by(id: 2)
+
+# puts @store1.name
+@store1.name = "Metropolis at Metrotown"
+# puts "after updating"
+# puts @store1.name
